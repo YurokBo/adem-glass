f9c4e53fb78487211292b2ee84648aadb9831f67
YurokBo <vertel3012@mail.ru>
2021-05-13 20:21:19 +0300

    create project

---
+++ src/utils/openModal.js
@@ -0,0 +1,30 @@
+import Works from "@/components/Works";
+
+export default function showModal(itemInfo) {
+    this.$modal.show(
+        {
+            template: `
+              <div name="modal" class="Modal Modal--bg" @close="$emit('close')">
+              <div class="Modal-Menu">
+                <div class="Modal-Logo"></div>
+                <button type="button" class="Modal-Close Btn" @click="$emit('close')">
+                  <span></span>
+                  <span></span>
+                </button>
+              </div>
+              <Works :works="itemInfo"/>
+              </div>
+            `,
+            props: ['itemInfo'],
+            components: {Works},
+        },
+        {itemInfo},
+        {
+            width: this.width > 410 ? 400 : 320,
+            height: 'auto',
+            adaptive: true,
+            scrollable: true,
+        },
+        {},
+    )
+}
