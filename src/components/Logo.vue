b6ff02be05997a70364fcec064eddab2d972e864
YurokBo <vertel3012@mail.ru>
2021-05-18 10:49:33 +0300

    main styles logo

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -18,25 +18,41 @@ export default {
 .Logo {
   display: flex;
   align-items: center;
+  font-family: 'Museo Sans Cyrl', sans-serif;
+  font-weight: 300;
 
   &-Title {
-    font-family: 'Museo Sans Cyrl', sans-serif;
-    font-weight: 300;
+    margin-right: 18px;
     font-size: 48px;
     line-height: 1.35em;
     letter-spacing: .022em;
-    color: var(--color-text-logo);
+    background: -webkit-linear-gradient(-90deg, rgba(253,227,176,1) 0%, rgba(197,142,70,1) 65%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+
+  &-Text {
+    position: relative;
+    padding-left: 20px;
+    span {
+      display: block;
+      font-size: 16px;
+      line-height: 1.2em;
+      letter-spacing: .022em;
+      color: var(--color-text-main);
+    }
+    &:before {
+      content: '';
+      display: block;
+      position: absolute;
+      width: 2px;
+      height: 36px;
+      top: 0;
+      left: 0;
+      transform: translate(-50%,0);
+      background-color: var(--color-bg-logo);
+      //box-shadow: 0px 0px 1px var(--color-bg-logo), 0 0 25px var(--color-bg-logo), 0 0 4px var(--color-bg-logo);
+    }
   }
 }
 </style>
-.ADEM_GLASS {
-font-size: 48px;
-font-family: "MuseoSansCyrl";
-color: rgb(253, 247, 242);
-line-height: 1.354;
-text-align: center;
-position: absolute;
-left: -1.063px;
-top: -14.496px;
-z-index: 1;
-}
